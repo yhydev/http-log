dd07dbcfa2fcf74b660d6f50cb06b06255bb2d5f
yhydev <31916483+yhydev@users.noreply.github.com>
2023-07-17 08:47:29 +0000

    update log

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
-
+from loguru import logger
 app = Flask(__name__)
 
 @app.route("/json", methods=["POST"])
 def index():
-    print("-----------------------\n%s\n%s%s" % (request.path, request.headers, request.json))
+    logger.info("-----------------------")
+    logger.info("%s\n%s" % (request.path, request.headers))
+    logger.info(request.json)
     return jsonify({});
 if __name__ == '__main__':
     app.run(debug=True)
